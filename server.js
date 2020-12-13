49388e47021a2ca1a0e3211616c19808a4a6acc4
sonphan18071999 <43442285+sonphan18071999@users.noreply.github.com>
2020-12-13 23:15:35 +0700

    make it better

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-var engine = require('consolidate');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/TLCN-Client-Angular-Blog'));
-// app.use(express.static('./TLCN-Client-Angular-Blog/src'));
-// app.use(express.static(__dirname+'TLCN-Client-Angular-Blog'))
-// app.use(express.static(path.join(__dirname, 'TLCN-Client-Angular-Blog/src/')));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/'));
 
-// app.set('view engine', 'html');
-// app.engine('html', engines.mustache);
-app.set('views', __dirname + '/src/');
-app.engine('html', engine.mustache);
-app.set('view engine', 'html');
-app.get('/index', function(req,res) {
-    res.render('index.html');
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
-// res.sendFile(path.join(__dirname+'/dist/TLCN-Client-Angular-Blog/index.html'));
-// });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080)
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
