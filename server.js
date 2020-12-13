0883e249209216201aaf7f38925c6ee0b3cf6acc
sonphan18071999 <43442285+sonphan18071999@users.noreply.github.com>
2020-12-13 17:37:02 +0700

    hello world

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/TLCN-Client-Angular-Blog'));
-
+// app.use(express.static(__dirname + '/dist/TLCN-Client-Angular-Blog'));
+app.use(express.static(__dirname + 'TLCN-Client-Angular-Blog'));
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/TLCN-Client-Angular-Blog/index.html'));
+res.sendFile(path.join(__dirname+'TLCN-Client-Angular-Blog/index.html'));
 });
+
+// res.sendFile(path.join(__dirname+'/dist/TLCN-Client-Angular-Blog/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
